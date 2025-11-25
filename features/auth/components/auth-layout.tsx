114d5a1587e9adf8beee3d9876c0217a10bf6742
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-25 14:09:47 +0000

    feat: add AuthLayout component for authentication pages

---
+++ features/auth/components/auth-layout.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col justify-center items-center p-6 gap-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link href="/" className="flex items-center self-center font-medium">
+          <Image src="/logos/logo.svg" alt="Logo" width={30} height={30} />
+          Flow Pilot
+        </Link>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
