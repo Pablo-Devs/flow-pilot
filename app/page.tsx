39414c55f0ccc9c4838ee65fec535ea81d4bb5f5
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-26 13:14:45 +0000

    refactor: clean up formatting and improve readability in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,29 +8,42 @@ import { toast } from "sonner";
 
 const Home = () => {
   const trpc = useTRPC();
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const testAi = useMutation(trpc.testAi.mutationOptions({
-    onSuccess: () => {
-      toast.success("AI Job queued");
-    }
-  }))
+  const testAi = useMutation(
+    trpc.testAi.mutationOptions({
+      onSuccess: () => {
+        toast.success("AI Job queued");
+      },
+      onError: () => {
+        toast.error("Something went wrong: ");
+      },
+    })
+  );
 
-  const create = useMutation(trpc.createWorkflow.mutationOptions({
-    onSuccess: () => {
-      toast.success("Job queued");
-    }
-  }));
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued");
+      },
+    })
+  );
   return (
     <div className="min-h-screen min-w-screen flex flex-col items-center justify-center gap-y-6">
       Proteccted Server Component
-      <div>
-        {JSON.stringify(data, null, 2)}
-      </div>
-      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>Test AI</Button>
-
-      <Button disabled={create.isPending} onClick={() => {create.mutate()}}>Create Workflow</Button>
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        Test AI
+      </Button>
+      <Button
+        disabled={create.isPending}
+        onClick={() => {
+          create.mutate();
+        }}
+      >
+        Create Workflow
+      </Button>
     </div>
   );
 };
